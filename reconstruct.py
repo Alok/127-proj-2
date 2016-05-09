46e1c09b5f4f02a3249a75bd02128200f6773d65
Alok Singh <alokbeniwal@gmail.com>
2016-05-08 18:42:31 -0700

    Change reconstruct

--- reconstruct.py
+++ reconstruct.py
@@ -4,7 +4,6 @@ import math
 import random
 import os
 import sys
-import subprocess
 import functools
 import itertools
 
@@ -18,38 +17,35 @@ from numpy.fft import ifft
 from numpy.fft import ifft2
 from numpy.fft import ifftshift
 
+# TODO someone implement interpolation or explain it to me so I can do it
+# properly
 
-# i choose to let the molecule sit in the cube [-1,1]^3
+# We choose to let the molecule sit in the cube [-1,1]^3.
 D = 1
 
 
-def b(v, F, idx):
+def b(v, F, I, idx):
     x_i = np.dot(v, F[idx][:, 0])
     y_i = np.dot(v, F[idx][:, 1])
     z_i = np.dot(v, F[idx][:, 2])
     vec = [x_i, y_i, z_i]
-    return vec
+
+    return ifft(fft(I[idx] * fft(l(vec))))
 
 
-def l(x, y, z):
-    def rect(a):
-        return 1 if (np.abs(a) <= D) else 0
+def l(v):
+    x, y, z = unpack(v)
 
-    def delta(a, b):
-        return np.infty if (a**2 + b**2 == 1) else 0
+    def rect(alpha):
+        return 1 if (np.abs(alpha) <= D) else 0
+
+    def delta(alpha, beta):
+        return np.infty if (alpha**2 + beta**2 == 1) else 0
     return delta(x, y) * rect(z)
 
 
 def r_hat(r):
-
     return fftshift(fft(r))
-
-# inv FFT of {B(x,y,z) / H(x,y,z)}
-# x_i = (x,y,z) * F[:,0]
-# y_i = (x,y,z) * F[:,1]
-# z_i = (x,y,z) * F[:,2]
-# B = sum over all imgs of FFT(b_i)
-# H = sum over imgs of D * sinc
 
 
 def unpack(v):
@@ -59,15 +55,15 @@ def unpack(v):
     return x, y, z
 
 
-def B(v, F):
+def B(v, F, I):
     N = len(F)
-    return np.sum([fft(b(v, F, i)) for i in range(N)])
+    return np.sum([fft(b(v, F, I, i)) for i in range(N)])
 
 
-def H(v, F):
+def H(v, F, I):
     N = len(F)
     return np.sum([D * np.sinc(D * math.pi * np.dot(v, F[i][:, 2])) for i in range(N)])
 
 
-def p(v, F):
-    return ifft(B(v, F) / H(v, F))
+def p(v, F, I):
+    return ifft(B(v, F, I) / H(v, F, I))
